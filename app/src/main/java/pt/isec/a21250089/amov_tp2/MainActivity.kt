8fc959640ef4838f54cd06ccc2365605583719d0
gustavolemos15 <guslemos1515@gmail.com>
2021-01-01 16:57:57 +0000

    Init

---
+++ app/src/main/java/pt/isec/a21250089/amov_tp2/MainActivity.kt
@@ -0,0 +1,11 @@
+package pt.isec.a21250089.amov_tp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
