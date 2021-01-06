2cb435d19e13947a9524311b08d2b460c22b85df
gustavolemos15 <guslemos1515@gmail.com>
2021-01-06 19:16:29 +0000

    Dialog para server mode e client mode a funcionar

--- app/src/main/java/pt/isec/a21250089/amov_tp2/GameActivity.kt
+++ app/src/main/java/pt/isec/a21250089/amov_tp2/GameActivity.kt
@@ -1,9 +1,16 @@
 package pt.isec.a21250089.amov_tp2
 
+import android.content.DialogInterface
 import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.Spanned
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.dialog_server_mode.*
@@ -37,7 +44,7 @@ class GameActivity : AppCompatActivity() {
         if (model.connectionState.value != GameViewModel.ConnectionState.CONNECTION_ESTABLISHED) {
             when (intent.getIntExtra("mode", SERVER_MODE)) {
                 SERVER_MODE -> startAsServer()
-                //CLIENT_MODE -> startAsClient()
+                CLIENT_MODE -> startAsClient()
             }
         }
 
@@ -54,20 +61,67 @@ class GameActivity : AppCompatActivity() {
         )
 
 
-
-        dlg = AlertDialog.Builder(this).run {
-            setTitle("Server Mode")
-            setView(R.layout.dialog_server_mode)
-            setOnCancelListener {
-                //model.stopServer()
-                finish()
-            }
-            create()
-        }
-        //model.startServer()
-        if(tvServerDialog != null)
-            tvServerDialog.text = String.format("IP: ", strIPAddress)
-        dlg?.show()
+        var dlg = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        dlg.setTitle("With EditText")
+        val dialogLayout = inflater.inflate(R.layout.dialog_server_mode, null)
+        val textView  = dialogLayout.findViewById<TextView>(R.id.tvServerDialog)
+        textView.text = "IP: ${strIPAddress}"
+        dlg.setView(dialogLayout)
+        dlg.show()
     }
 
+    private fun startAsClient() {
+            var filtros = arrayOf(object : InputFilter {
+                override fun filter(
+                        source: CharSequence?,
+                        start: Int,
+                        end: Int,
+                        dest: Spanned?,
+                        dstart: Int,
+                        dend: Int
+                ): CharSequence? {
+                    if (source?.none { it.isDigit() || it == '.' } == true)
+                        return ""
+                    return null
+                }
+            })
+
+        var dl = AlertDialog.Builder(this)
+        dl.setTitle("Client Modde")
+        dl.setMessage("IP: ")
+        val inflater = layoutInflater
+        val dialogLayout = inflater.inflate(R.layout.dialog_client_mode, null)
+        var btnPos = dialogLayout.findViewById<Button>(R.id.btnClientPos)
+        var btnNeutro = dialogLayout.findViewById<Button>(R.id.btnClientNeutral)
+        var btnNeg = dialogLayout.findViewById<Button>(R.id.btnClientNeg)
+        var edit = dialogLayout.findViewById<EditText>(R.id.etClientDialog)
+        edit.filters = filtros
+        btnPos.setOnClickListener(){ v ->
+            val strIP = edit.text.toString()
+            if (strIP.isEmpty() || !Patterns.IP_ADDRESS.matcher(strIP).matches()) {
+                Toast.makeText(this@GameActivity, "Erro", Toast.LENGTH_LONG).show()
+                finish()
+            } else {
+                //model.startClient(edtBox.text.toString())
+            }
+        }
+        btnNeutro.setOnClickListener(){ v ->
+                // model.startClient("10.0.2.2", SERVER_PORT-1)
+                // Add port redirect on the Server Emulator:
+                // telnet localhost <5554|5556|5558|...>
+                // auth <key>
+                // redir add tcp:9998:9999
+            }
+        btnNeg.setOnClickListener(){ v ->
+                finish()
+            }
+        dl.setView(dialogLayout)
+        dl.show()
+    }
 }
+
+private fun Button.setOnClickListener(string: String.Companion) {
+
+}
+
